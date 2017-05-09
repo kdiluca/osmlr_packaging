ea74101c5e0cd8a0b2547806bafbb17afe79d5be
Matt Amos <zerebubuth@gmail.com>
2017-05-09 16:18:05 +0100

    Fix publishing script.

--- publish.sh
+++ publish.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -e
 
+if [ -z ${LAUNCHPADUSER+x} ]; then
+    echo "\$LAUNCHPADUSER not set, please set this to your Launchpad user name."
+    exit 1
+fi
+
+if [ -z ${1+x} ]; then
+    echo "Usage: $0 [distribution,distribution,...]"
+    echo "For example: $0 trusty,xenial"
+    exit 1
+fi
+
 #get all of the packages ready
 NO_BUILD=true ./package.sh "${1}"
 IFS=',' read -r -a DISTRIBUTIONS <<< "${1}"
@@ -11,13 +22,13 @@ cd ${DISTRIBUTIONS[0]}/unpinned
 bzr init
 bzr add
 bzr commit -m "Packaging for ${VERSION}-0ubuntu1."
-bzr push --overwrite bzr+ssh://valhalla-routing@bazaar.launchpad.net/~valhalla-routing/+junk/osmlr_${VERSION}-0ubuntu1
+bzr push --overwrite bzr+ssh://${LAUNCHPADUSER}@bazaar.launchpad.net/~valhalla-core/+junk/osmlr_${VERSION}-0ubuntu1
 cd -
 
 #sign and push each package to launchpad
 for dist in ${DISTRIBUTIONS[@]}; do
 	for pin in pinned unpinned; do
 		debsign ${dist}/${pin}/*source.changes
-		dput ppa:valhalla-routing/osmlr ${dist}/${pin}/*source.changes
+		dput ppa:valhalla-core/opentraffic ${dist}/${pin}/*source.changes
 	done
 done
